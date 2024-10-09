aafca19bf2f579616c2642b742c9a31991f79541
Owen Newberry <owen@owennewberry.com>
2024-10-09 15:01:53 -0400

    working on form handling

--- entity/create.php
+++ entity/create.php
@@ -10,6 +10,47 @@ $recipe = null;
 
 $title = 'Create a Recipe';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $category = $_POST['category'];
+  //$author = $_POST['author'];
+  //Will be tied to account holder
+  $image = $_POST['image'];
+  $prep_time_hours = $_POST['prep_time_hours'];
+  $prep_time_minutes = $_POST['prep_time_minutes'];
+  $cook_time_hours = $_POST['cook_time_hours'];
+  $cook_time_minutes = $_POST['cook_time_minutes'];
+  $servings = $_POST['servings'];
+  //$ingredients = $_POST['ingredients'];
+  //$steps = $_POST['steps'];
+
+  $total_time_hours = $prep_time_hours + $cook_time_hours;
+  $total_time_minutes = $prep_time_minutes + $cook_time_minutes;
+  if ($total_time_minutes >= 60) {
+    $total_time_hours++;
+    $total_time_minutes = $total_time_minutes % 60;
+  }
+
+  $new_recipe = [
+    'id' => count($recipes) + 1,
+    'name' => $name,
+    //'author' => $author,
+    //Will be tied to account holder
+    'category' => $category,
+    'image' => $image,
+    'prep_time_hours' => $prep_time_hours,
+    'prep_time_minutes' => $prep_time_minutes,
+    'cook_time_hours' => $cook_time_hours,
+    'cook_time_minutes' => $cook_time_minutes,
+    'total_time' => "{$total_time_hours} hours {$total_time_minutes} minutes",
+    'servings' => $servings,
+    //'ingredients' => $ingredients,
+    //'steps' => $steps,
+  ];
+
+  print_r($new_recipe);
+}
+
 // Uncomment for testing purposes:
 // echo '<pre>';
 // print_r($_POST);
